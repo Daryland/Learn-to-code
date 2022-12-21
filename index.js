6d849b14067f665bdfec28c83d7eaff7596ab695
foesublime <rylandcs@gmail.com>
2022-12-21 13:09:54 -0700

    JS index added

---
+++ index.js
@@ -0,0 +1,9 @@
+
+    // var name = 'Daniel';
+    var lastName = 'Ryland';
+
+    var fullName = name + ' ' + lastName;
+
+    console.log(fullName);
+    console.log(name);
+    console.log('Hello World');
